c5c6d96f50ae084d276ece7139278653f6b73271
krutzika <kruthika.ramesh99@gmail.com>
2025-06-19 21:59:31 +0100

    fix: Rectified SQL model

--- models/user.py
+++ models/user.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 from sqlmodel import Field,SQLModel
+from typing import Optional
 
 class User(SQLModel, table=True):
-    id : int |  Field(default=None,  primary_key=True, index=True)
-    email : str |  Field(default=None,  unique=True, nullable=False)
+    id : Optional[int] = Field(default=None,  primary_key=True, index=True)
+    email : str = Field(default=None,  unique=True, nullable=False)
     hashed_password: str = Field(nullable=False)
     created_at: datetime = Field(default_factory=datetime.utcnow)
 
